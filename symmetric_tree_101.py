74f515a2f0c116abac25784473cb17cdccd4f005
unknown <ggoli@tenaroncapital.com>
2019-03-19 12:36:23 -0400

    Implemented iteratively with bfs

--- symmetric_tree_101.py
+++ symmetric_tree_101.py
@@ -10,7 +10,19 @@ def isSymmetric(root):
     if root is None: return True
     return isMirror(root.left, root.right)
 
+def isSymmetric2(root):
+    if root is None: return True
+    stack = [(root.left, root.right)]
+    while stack:
+        left, right = stack.pop()
+        if left is None and right is None: continue
+        if left is None or right is None: return False
+        if left.val != right.val: return False
+        stack.append((left.left, right.right))
+        stack.append((left.right, right.left))
+    return True
+
 if __name__ == '__main__':
-    root = listtoTreeNode([1])
+    root = listtoTreeNode([1,2,2,None,4,4,None])
     prettyPrintTree(root)
-    print(isSymmetric(root))
+    print(isSymmetric2(root))

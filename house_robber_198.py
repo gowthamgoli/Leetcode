0499823a4ba0aa787d180061d6bebfd3ed4bf7ca
gowthamgoli <sgg308@nyu.edu>
2019-03-20 15:14:26 -0400

    O(1) space DP solution

--- house_robber_198.py
+++ house_robber_198.py
@@ -1,12 +1,12 @@
 def rob(nums):
     n = len(nums)
     if n == 0: return 0
-    p = [0] * (n + 1)
-    p[0], p[1] = 0, nums[0]
-    for i in range(2, n + 1):
-        p[i] = max(p[i-2] + nums[i - 1], p[i - 1])
-    # print(p)
-    return p[n]
+    prev_prev_max = 0
+    prev_max = 0
+    for i in range(n):
+        current_max = max(prev_prev_max + nums[i], prev_max)
+        prev_prev_max, prev_max  = prev_max, current_max 
+    return current_max
 
 if __name__ == '__main__':
     print(rob([1,2,3,1]))

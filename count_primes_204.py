f9c00b8d75cdb3ea78a74a8aadefcee66cad31d0
gowthamgoli <sgg308@nyu.edu>
2019-03-21 11:00:08 -0400

    Implemented Sieve of Eratosthenes

--- count_primes_204.py
+++ count_primes_204.py
@@ -1,15 +1,16 @@
 import math
 
 def countPrimes(n):
-    def isPrime(k):
-        p = int(math.sqrt(k))
-        for i in range(2, p + 1):
-            if k % i == 0: return False
-        return True
-    output = []
-    for i in range(2, n):
-        if isPrime(i): output.append(i)
-    return len(output)
+    output = 0
+        if n in [0, 1]: return 0
+        primes = [1] * n
+        primes[0] = primes[1] = 0
+        for i in range(2, n // 2 + 1):
+            if primes[i] == 0: continue
+            for j in range(i * i, n, i):
+                primes[j] = 0
+        for i in range(2, n): output += primes[i]
+        return output
 
 if __name__ == '__main__':
-    print(countPrimes(10))
+    print(countPrimes(39))

08d03b8d95ad5d58139124737ae0703af9341a16
gowthamgoli <sgg308@nyu.edu>
2019-03-21 09:26:32 -0400

    Implementation using two stacks

--- min_stack_155.py
+++ min_stack_155.py
@@ -1,10 +1,10 @@
 class MinStack:
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        stack = []
-        min_stack = []
+        self.stack = []
+        self.min_stack = []
+
+    def __str__(self):
+        return f"{self.stack} | {self.min_stack}"
 
     def push(self, x):
         self.stack.append(x)
@@ -16,17 +16,26 @@ class MinStack:
             self.min_stack.append(x)
 
     def pop(self):
-        self.stack.pop()
+        x = self.stack.pop()
+        if self.min_stack and x == self.min_stack[-1]:
+            self.min_stack.pop()
 
     def top(self):
-        return self.stack[-1]
+        return self.stack[-1] if self.stack else None
 
     def getMin(self):
-        return self.min_stack[-1]
+        return self.min_stack[-1] if self.min_stack else None
         
 # Your MinStack object will be instantiated and called as such:
-obj = MinStack()
-obj.push(2)
-obj.pop()
-param_3 = obj.top()
-param_4 = obj.getMin()
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+minStack.push(-3)
+print(minStack)
+print(minStack.getMin())
+minStack.pop()
+print(minStack)
+print(minStack.top())
+print(minStack)
+print(minStack.getMin())

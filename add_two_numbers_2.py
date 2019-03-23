afa3673f05dcafcf85ea9f7c68b471908c9dab59
gowthamgoli <gowthamgoli1@gmail.com>
2019-03-23 19:06:10 -0400

    renamed and optimized code

--- add_two_numbers_2.py
+++ add_two_numbers_2.py
@@ -31,19 +31,34 @@ def addTwoNumbers(l1, l2):
 
     return dummy_root.next
 
+def addTwoNumbers2(l1, l2):
+    dummy_root = ListNode(0)
+    ptr = dummy_root
+    carry = 0
+    while l1 or l2 or carry:
+        l1_val = l1.val if l1 else 0
+        l2_val = l2.val if l2 else 0
+        s = l1_val + l2_val + carry
+        ptr.next = ListNode(s % 10)
+        carry = s // 10
+        if l1: l1 = l1.next
+        if l2: l2 = l2.next
+        ptr = ptr.next
+    # print(listNodeToList(dummy_root.next))
+    return dummy_root.next
 
 if __name__ == '__main__':
     l1 = listtoListNode([2,4,3])
     l2 = listtoListNode([5,6,4])
-    res = addTwoNumbers(l1, l2)
+    res = addTwoNumbers2(l1, l2)
     print(listNodeToList(res))
 
     l1 = listtoListNode([2,4,9, 9, 9, 9])
     l2 = listtoListNode([5,6,4])
-    res = addTwoNumbers(l1, l2)
+    res = addTwoNumbers2(l1, l2)
     print(listNodeToList(res))
 
-    l1 = listtoListNode([])
-    l2 = listtoListNode([])
-    res = addTwoNumbers(l1, l2)
-    print(listNodeToList(res))
+    # l1 = listtoListNode([])
+    # l2 = listtoListNode([])
+    # res = addTwoNumbers(l1, l2)
+    # print(listNodeToList(res))

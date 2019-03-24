fc3001caf4ebdf401f8fb0c4a409bbf8bafc14fe
gowthamgoli <gowthamgoli1@gmail.com>
2019-03-24 14:05:28 -0400

    Using level order traversal

---
+++ populating_next_right_pointers_in_each_node_116.py
@@ -0,0 +1,27 @@
+from BinaryTree import prettyPrintTree, listtoTreeNode
+
+def connect(root):
+    levels = []
+    if root is None: return None
+    queue = []
+    queue.append(root)
+    while len(queue) > 0:
+        level = []
+        n = len(queue)
+        for _ in range(n):
+            node = queue.pop(0)
+            level.append(node)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+        levels.append(level)
+
+    for i in range(len(levels)):
+        for j in range(0, len(levels[i]) - 1):
+            levels[i][j].next = levels[i][j+1]
+
+    return levels
+
+if __name__ == '__main__':
+    root = listtoTreeNode([1,2,3,4,None,None,5])
+    prettyPrintTree(root)
+    print(connect(root))

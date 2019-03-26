8384a0f8aabf60e5177d57efe86ecb447c219d12
gowthamgoli <gowthamgoli1@gmail.com>
2019-03-25 21:39:31 -0400

    backtracking solution

--- permutations_46.py
+++ permutations_46.py
@@ -1,21 +1,18 @@
 def permute(nums):
-
     def backtrack(output, permutation, nums):
-        print(permutation)
         if len(permutation) == len(nums):
-            output.append(permutation)
+            output.append(permutation[:])
             return
-        
+
         for num in nums:
-            if num not in permutation: permutation.append(num)
+            if num in permutation:
+                continue
+            permutation.append(num)
             backtrack(output, permutation, nums)
             permutation.pop()
-
     output = []
-    permutation = []
-    backtrack(output, permutation, nums)
-    print(output)
-    return
+    backtrack(output, [], nums)
+    return output
 
 if __name__ == "__main__":
     print(permute([1,2,3]))

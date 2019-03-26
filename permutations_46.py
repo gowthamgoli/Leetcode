e42db6c07f4f2d68ac7c509427aefabe97ac2a0f
gowthamgoli <gowthamgoli1@gmail.com>
2019-03-25 23:37:49 -0400

    chane

--- permutations_46.py
+++ permutations_46.py
@@ -1,6 +1,7 @@
 def permute(nums):
     def backtrack(output, permutation, nums):
         if len(permutation) == len(nums):
+            # print(permutation)
             output.append(permutation[:])
             return
 
@@ -8,8 +9,10 @@ def permute(nums):
             if num in permutation:
                 continue
             permutation.append(num)
+            print(permutation)
             backtrack(output, permutation, nums)
             permutation.pop()
+            print(permutation)
     output = []
     backtrack(output, [], nums)
     return output

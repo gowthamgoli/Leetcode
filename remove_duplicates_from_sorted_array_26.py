7b5156d5f6ad9186868703bdb9cb1d63535e790c
gowthamgoli <sgg308@nyu.edu>
2019-03-27 10:10:22 -0400

    Refactored code

--- remove_duplicates_from_sorted_array_26.py
+++ remove_duplicates_from_sorted_array_26.py
@@ -1,20 +1,15 @@
 def removeDuplicates(nums):
-    if len(nums) == 1: return 1
-    i = 0
-    j = 1
-    while i < len(nums) and j < len(nums):
-        while j < len(nums) and nums[i] == nums[j]:
-            j += 1
-        if j == len(nums): break
-        nums[i + 1], nums[j] = nums[j], nums[i + 1]
+    if len(nums) == 0: return 0
+    i, j = 0, 0
+    for j in range(len(nums)):
+        if nums[i] == nums[j]: continue
         i += 1
-        j += 1
-    nums = nums[0: i + 1]
-    print(nums)
-    return len(nums)
+        nums[i] = nums[j]
+    print(nums[: i + 1])
+    return i + 1
 
 if __name__ == '__main__':
-    print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
-    print(removeDuplicates([1, 2, 3, 4]))
-    print(removeDuplicates([1, 2]))
-    print(removeDuplicates([1, 1]))
+    # print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+    # print(removeDuplicates([1, 2, 3, 4]))
+    # print(removeDuplicates([1, 1]))
+    print(removeDuplicates([]))

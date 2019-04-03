49dc89cd3e291f98d605ab7a2554f1e5a1ca4d08
gowthamgoli <gowthamgoli1@gmail.com>
2019-04-02 23:17:51 -0400

    O(mn) time, O(n) space

--- unique_paths_62.py
+++ unique_paths_62.py
@@ -8,6 +8,13 @@ def uniquePaths(m, n):
                 paths[i][j] = paths[i - 1][j] + paths[i][j-1]
     return paths[n-1][m-1]
 
+def uniquePaths2(m, n):
+    paths = [1] * m
+    for i in range(1, n):
+        for j in range(1, m):
+            paths[j] = paths[j] + paths[j - 1]
+    return paths[-1]
+
 if __name__ == "__main__":
-    print(uniquePaths(3, 2))    
-    print(uniquePaths(7, 3))    
+    print(uniquePaths2(3, 2))    
+    print(uniquePaths2(7, 3))    

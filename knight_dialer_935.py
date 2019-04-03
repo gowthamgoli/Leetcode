cd6669140ccf44f3cc01e2ac5e642b3ec1ab3056
gowthamgoli <sgg308@nyu.edu>
2019-04-03 11:43:40 -0400

    Naive DFS solution time limit exceeded

--- knight_dialer_935.py
+++ knight_dialer_935.py
@@ -21,8 +21,35 @@ def knightDialer(N):
                     paths[n][i][j] += temp
     return sum(map(sum, paths[N-1])) % int(math.pow(10, 9) + 7)
 
+def knightDialer2(N):
+    graph = {
+        0: [4, 6],
+        1: [6, 8],
+        2: [7, 9],
+        3: [4, 8],
+        4: [0, 3, 9],
+        5: [],
+        6: [0, 1, 7],
+        7: [2, 6],
+        8: [1, 3],
+        9: [2, 4],
+    }
+    
+    def dfs(graph, node, level, N):
+        count = 0
+        if level == N:
+            return 1
+        for neighbor in graph[node]:
+            count += dfs(graph, neighbor, level + 1, N)
+        return count
+
+    output = 0
+    for node, _ in graph.items():
+        output += dfs(graph, node, 1, N)
+    return output % int(math.pow(10, 9) + 7)
+
 if __name__ == "__main__":
     # print(knightDialer(3))
-    # print(knightDialer(2))
-    # print(knightDialer(1))
-    print(knightDialer(161))
+    # print(knightDialer2(2))
+    # print(knightDialer2(17))
+    print(knightDialer2(161))

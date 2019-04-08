19ca4599e7660b8f963fa417b076a9e859473389
gowthamgoli <sgg308@nyu.edu>
2019-04-08 17:47:12 -0400

    O(1) using dict and double linked list

--- lru_cache_146.py
+++ lru_cache_146.py
@@ -1,3 +1,10 @@
+class DoubleLinkedList:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -35,16 +42,64 @@ class LRUCache:
     def __str__(self):
         return f'{self.cache} \n {self.mru}'
 
+class LRUCache2:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = DoubleLinkedList(-1, -1)
+        self.tail = DoubleLinkedList(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            n = self.cache[key]
+            self._remove(n)
+            # n = DoubleLinkedList(key, self.cache[key].val)
+            self._add(n)
+            return n.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        n = DoubleLinkedList(key, value)
+        self._add(n)
+        self.cache[key] = n
+        if len(self.cache) > self.capacity:
+            n = self.head.next
+            del self.cache[n.key]
+            self._remove(n)
+
+    def _add(self, n):
+        temp = self.tail.prev
+        temp.next = n
+        n.prev = temp
+        n.next = self.tail
+        self.tail.prev = n
+
+    def _remove(self, n):
+        n_prev = n.prev
+        n_next = n.next
+        n_prev.next = n_next
+        n_next.prev = n_prev
+
+    def __str__(self):
+        return f'{self.cache}'
+
 if __name__ == "__main__":
     # Your LRUCache object will be instantiated and called as such:
     capacity = 2
-    obj = LRUCache(capacity)
+    obj = LRUCache2(capacity)
     param_1 = obj.get(2)
-    obj.put(2,6)
+    obj.put(1,1)
+    obj.put(2,2)
     obj.get(1)
-    obj.put(1, 5)
-    obj.put(1, 2)
-    obj.get(1)
-    obj.get(2)
-    # print(obj.get(2))
-    print(obj)
+    obj.put(3,3)
+    print(obj.get(2))
+    # obj.put(1, 5)
+    # obj.put(1, 2)
+    # obj.get(1)
+    # obj.get(2)
+    # print(obj)

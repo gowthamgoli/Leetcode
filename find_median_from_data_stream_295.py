4ec051a13e902aa6c0e747975f4efde547538c40
gowthamgoli <sgg308@nyu.edu>
2019-04-11 16:24:22 -0400

    Implementation using heap

--- find_median_from_data_stream_295.py
+++ find_median_from_data_stream_295.py
@@ -1,4 +1,5 @@
 import bisect
+import heapq
 
 class MedianFinder:
 
@@ -21,8 +22,27 @@ class MedianFinder:
     def __str__(self):
         return str(self.nums)
 
+class MedianFinder2:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+        if len(self.small) > len(self.large):
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+    def findMedian(self) -> float:
+        if len(self.large) == len(self.small):
+           return  float((self.large[0] - self.small[0]) / 2.0)
+        return float(self.large[0])
+
+
 # Your MedianFinder object will be instantiated and called as such:
-obj = MedianFinder()
+obj = MedianFinder2()
 obj.addNum(1)
 obj.addNum(2)
 obj.addNum(3)

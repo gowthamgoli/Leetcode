0774cbd048883ff829f4a3df17dbd3535cdcb595
gowthamgoli <sgg308@nyu.edu>
2019-04-12 10:53:48 -0400

    O(1) solution

--- design_tic_tac_toe_348.py
+++ design_tic_tac_toe_348.py
@@ -50,8 +50,39 @@ class TicTacToe:
             output += '|'.join(row) + '\n'
         return output
 
+class TicTacToe2:
 
-toe = TicTacToe(3)
+    def __init__(self, n: int):
+        """
+        Initialize your data structure here.
+        """
+        self.rows, self.cols = [0] * n, [0] * n
+        self.diag, self.antidiag = 0, 0
+        self.n = n
+
+    def move(self, row: int, col: int, player: int) -> int:
+        """
+        Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins.
+        """
+        offset = 2 * player - 3
+        self.rows[row] += offset
+        self.cols[col] += offset
+        if row == col: self.diag += offset
+        if row + col == self.n - 1: self.antidiag += offset
+        if self.n in [self.rows[row], self.cols[col], self.diag, self.antidiag]:
+            return 2
+        if -self.n in [self.rows[row], self.cols[col], self.diag, self.antidiag]:
+            return 1
+        return 0
+
+toe = TicTacToe2(3)
 
 print(toe.move(0, 0, 1))
 print(toe.move(0, 2, 2))

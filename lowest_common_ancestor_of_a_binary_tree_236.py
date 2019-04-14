5d37a4856184e74bc785adc5b2f7787d19005302
gowthamgoli <gowthamgoli1@gmail.com>
2019-04-13 22:28:38 -0400

    append to path instead of creating new list reduced time

--- lowest_common_ancestor_of_a_binary_tree_236.py
+++ lowest_common_ancestor_of_a_binary_tree_236.py
@@ -2,14 +2,15 @@ from BinaryTree import *
 
 def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
     def dfs(root, path, node):
-            if root is None: return
-
-            if root.val == node.val:
-                return path + [root]
-            # path.append(root.val)
-            path_left = dfs(root.left, path + [root], node)
-            path_right = dfs(root.right, path + [root], node)
-            return path_left or path_right
+        if root is None: return
+        path.append(root)
+        if root.val == node.val:
+            return path
+        path_left = dfs(root.left, path, node)
+        if path_left: return path_left
+        path_right = dfs(root.right, path, node)
+        if path_right: return path_right
+        path.pop()
 
     path_p = dfs(root, [], p)
     path_q = dfs(root, [], q)
@@ -18,6 +19,7 @@ def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -
     while i < n and path_p[i].val == path_q[i].val:
         i += 1
     return path_p[i - 1]
+
 
 if __name__ == "__main__":
     root = listtoTreeNode([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4])

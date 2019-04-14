8727d7c85b24a94d73fe54ec9f9dead368798090
gowthamgoli <gowthamgoli1@gmail.com>
2019-04-13 22:31:49 -0400

    Clever recursive solution but still O(n) time

--- lowest_common_ancestor_of_a_binary_tree_236.py
+++ lowest_common_ancestor_of_a_binary_tree_236.py
@@ -20,6 +20,15 @@ def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -
         i += 1
     return path_p[i - 1]
 
+def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if root is None: return None
+    if root.val == p.val or root.val == q.val: return root
+    
+    lca_left = lowestCommonAncestor(root.left, p, q)
+    lca_right = lowestCommonAncestor(root.right, p, q)
+
+    if lca_left and lca_right: return root
+    return lca_left if lca_left else lca_right
 
 if __name__ == "__main__":
     root = listtoTreeNode([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4])
